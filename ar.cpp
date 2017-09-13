464689ad9792564b79cd79bedfbdc121e08ec5b3
Foster <brownfosman5000@gmail.com>
2017-09-13 17:59:39 -0400

    Update

--- ar.cpp
+++ ar.cpp
@@ -3,23 +3,35 @@
 using namespace std;
 
 
-struct customer{
+struct Customer{
 	int customernumber;
 	string name;
 	double balance;
-
 };
 
+struct Record{
+	string transaction_type;
+	int transaction_num;
+	int num_item;
+	string item; 
+	double amount;
 
-
+};
 void checkOpen(const ifstream& ,const string file);
-int getNumberCustomers(ifstream&);
-void getAllCustomerInfo(ifstream&,customer[],const int);
+int getNumberLines(ifstream&);
+void getAllCustomerInfo(ifstream&,Customer[],const int);
+
+void processTransactions(Customer, Record);
+void printRecordInfo(Record);
+
+
+void processCustomer(ifstream&, const int,Customer[], Record);
 
 int main(){
 	ifstream master_in,transaction_in;
 	string cpmaster = "cpmaster.txt";string transaction = "transaction.txt";
 	int num_customers;
+	Record customer_record;
 	
 	master_in.open(cpmaster.c_str()); 	
 	transaction_in.open(transaction.c_str()); 	
@@ -27,14 +39,19 @@ int main(){
 	checkOpen(master_in,cpmaster);
 	checkOpen(transaction_in,transaction);		
 	
-	
-	num_customers = getNumberCustomers(master_in);
-	customer customer_arr[num_customers];	
+	//Get number of customers	
+	num_customers = getNumberLines(master_in);
+
+	Customer customer_arr[num_customers];	
 
 	getAllCustomerInfo(master_in,customer_arr,num_customers);
 
+	for(int current_customer = 0;current_customer<num_customers;current_customer++)
+		processCustomer(transaction_in,current_customer,customer_arr,customer_record); 
 }
-int getNumberCustomers(ifstream& fin){
+
+//Get number of lines in file
+int getNumberLines(ifstream& fin){
 	int numlines=0;	
 	string line;
 
@@ -59,7 +76,7 @@ void checkOpen(const ifstream &fin,const string file){
 }
 
 //Gets info from file to be stored in stored in each customer
-void getAllCustomerInfo(ifstream& fin,customer customer_arr[],const int num_customers){
+void getAllCustomerInfo(ifstream& fin,Customer customer_arr[],const int num_customers){
 	for(int i = 0;i<num_customers;i++){
 		fin >> customer_arr[i].customernumber;
 		fin >> customer_arr[i].name;
@@ -67,10 +84,47 @@ void getAllCustomerInfo(ifstream& fin,customer customer_arr[],const int num_cust
 	}
 }
 
-void 
-//void customerInvoice(){
-//}
+void processCustomer(ifstream& fin, const int current_customer,Customer customer_arr[], Record customer_record){
+	int customer_number = 0;	
+	//Get number of transactions
+	int transactions = getNumberLines(fin);
+	fin >> customer_number;
+	
+	//Var to pass around meaning current customer	
+	Customer customer = customer_arr[current_customer];
+
+	if (customer_number == customer.customernumber); 
+		for(int i = 0; i<transactions;i++){
+			fin >> customer_record.transaction_type;
+			fin >> customer_record.transaction_num;
+			fin >> customer_record.item;
+			fin >> customer_record.num_item;
+			fin >> customer_record.amount;
+			
+			processTransactions(customer,customer_record);	
+			printRecordInfo(customer_record);
+		}
+	}
+
+void processTransactions(Customer customer, Record customer_record){
+	cout<<customer_record.transaction_type<<endl;
+	if (customer_record.transaction_type == "O")
+		customer.balance += customer_record.amount;
+
+	else if(customer_record.transaction_type == "P")
+		customer.balance -= customer_record.amount;		
+
+	else
+		cerr<<"Error Processing customer record type ---  Needs to be either 'O' or 'P'"<<endl;
+
+}
+
+void printRecordInfo(Record current_record ){
+	cout<<"Transaction #"<< current_record.transaction_num << current_record.item <<"$"<<current_record.amount << endl; 
 
 
 
-
+}
+//void customerInvoice(){}
+//bool checkDuplicate(){}
+//bool checkRecords(){}
